6739618de6f72be97f968eb577362305b2e5eee0
21343055 <112170659+21343055@users.noreply.github.com>
2022-10-14 11:51:02 +0700

    Add files via upload

---
+++ Prak 8/src/Latihan/ArraySample.java
@@ -0,0 +1,13 @@
+/*
+* Nama = Muhamad Fathur Rahman
+* NIM  = 21343055
+* Prodi= Teknik Informatika
+ */
+public class ArraySample {
+    public static void main( String[]args){
+       int[] ages = new int[100];
+       for(int i = 0; i < 100;i++){
+           System.out.print(ages[i]);
+       }
+    }
+}
---
+++ Prak 8/src/Latihan/ArraySample3.java
@@ -0,0 +1,19 @@
+/*
+* Nama = Muhamad Fathur Rahman
+* NIM  = 21343055
+* Prodi= Teknik Informatika
+ */
+public class ArraySample3 {
+    public static void main( String[]args){
+        
+        //String Array 4 baris x 2 Kolom
+       String[][] dogs = {{"Terry","Brown"},//Baris ke 0
+           {"Kristin","White"},//Baris ke 1
+           {"Toby","Gray"},//Baris ke 2
+           {"Fido","Black"}//Baris ke 3
+       };
+        System.out.println(dogs[0][0]);
+        //Mengakses Vaiabel dogs dengan indeks baris 0, indeks kolom 0
+    }
+    
+}
